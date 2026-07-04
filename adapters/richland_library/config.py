d789617dffc366676673866bc1f1cef265b03390
iknowtheheimlich <137670366+iknowtheheimlich@users.noreply.github.com>
2026-07-04 15:13:25 -0700

    Attempt_18_Add_Richland_Library_LibCal_Config

--- adapters/richland_library/config.py
+++ adapters/richland_library/config.py
@@ -3,3 +3,8 @@
 SOURCE_NAME = "RichlandLibrary"
 DEFAULT_VENUE = "Richland Library"
 DEFAULT_CITY = "Richland"
+
+BASE_URL = "https://myrichlandlibrary.libcal.com"
+MONTHLY_ENDPOINT = f"{BASE_URL}/ajax/calendar/monthly"
+CALENDAR_ID = "87462"
+CALENDAR_CONTEXT = "19280"
