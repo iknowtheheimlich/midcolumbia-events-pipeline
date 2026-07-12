c68869768b0d758778bb8021818e42b395c8fe66
iknowtheheimlich <137670366+iknowtheheimlich@users.noreply.github.com>
2026-07-12 16:01:42 -0700

    Attempt_37 configure AllEvents collection pages

---
+++ adapters/allevents/config.py
@@ -0,0 +1,8 @@
+"""AllEvents collector configuration."""
+
+CITY_PAGES = {
+    "Kennewick": "https://allevents.in/kennewick/all",
+    "Pasco": "https://allevents.in/pasco/all",
+    "Richland": "https://allevents.in/richland/all",
+    "West Richland": "https://allevents.in/west-richland/all",
+}
