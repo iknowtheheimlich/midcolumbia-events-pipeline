c56895405eb54991daf87f1fb47063cad766b049
iknowtheheimlich <137670366+iknowtheheimlich@users.noreply.github.com>
2026-07-12 20:04:36 -0700

    Attempt_44: pass inspector options through Windows launcher

--- run_publish_reddit_live.bat
+++ run_publish_reddit_live.bat
@@ -3,9 +3,9 @@ setlocal
 cd /d "%~dp0"
 
 if "%~1"=="" (
-  echo Usage: run_publish_reddit_live.bat YYYY-MM-DD
+  echo Usage: run_publish_reddit_live.bat YYYY-MM-DD [publisher options]
   exit /b 2
 )
 
-python -m tools.publish_reddit_live --week-start %1
+python -m tools.publish_reddit_live --week-start %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
